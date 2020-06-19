70fb3c74638bfecddd6d2e7a8b7e3bfa2164e3b7
Cheng Xiang <cxiang@microstrategy.com>
2020-06-19 18:40:01 +0800

    optimize code view

--- kubernetes-dashboard-2/get_admin-token.sh
+++ kubernetes-dashboard-2/get_admin-token.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep kubernetes-dashboard-administrator | awk '{print $1}') | awk '{if($1=="token:"){print $2}}'
+service_account="kubernetes-dashboard-administrator"
+secret_name=$(kubectl -n kubernetes-dashboard get secret | grep $service_account | awk '{print $1}')
+kubectl -n kubernetes-dashboard describe secret "$secret_name" | awk '{if($1=="token:"){print $2}}'
